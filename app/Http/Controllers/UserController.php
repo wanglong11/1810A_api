8ef25015d1a5bca489bc1d07949311064947b351
wwl <you@example.com>
2019-06-06 00:27:47 +0000

    提交

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\UserModel;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
     //
     public function login(){
         //echo "111";
-      $data=UserModel::get()->toArray();
+      //$data=UserModel::get()->toArray();
         //dd($data);
+       //phpinfo();
+        echo "<pre>";
+        print_r($_SERVER);
+        echo "<pre>";
+
+
     }
 }
